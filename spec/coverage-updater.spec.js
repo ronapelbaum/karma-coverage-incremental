79e54e186fbf56b573442651af1450937312ecd6
Ron Apelbaum <ronapelbaum@gmail.com>
2017-04-27 10:03:07 +0300

    add istanbul-coveralls

--- spec/coverage-updater.spec.js
+++ spec/coverage-updater.spec.js
@@ -1,8 +1,8 @@
-const coverage_updater = require('../index')
-console.log('ron coverage-updater.spec.()', coverage_updater);//TODO remove this
+// const coverage_updater = require('../index')
+// console.log('ron coverage-updater.spec.()', coverage_updater);// TODO remove this
 
-describe('coverage-updater spec', ()=>{
-    it('should fail', ()=>{
-        expect(true).toBe(false);
-    });
+describe('coverage-updater spec', () => {
+  it('should fail', () => {
+    expect(true).toBe(false);
+  });
 });
