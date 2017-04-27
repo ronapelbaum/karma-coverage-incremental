224c7ffd6da00e108654fdc99031e265d2f347dd
Ron Apelbaum <ronapelbaum@gmail.com>
2017-04-27 10:31:55 +0300

    fix test

--- spec/coverage-updater.spec.js
+++ spec/coverage-updater.spec.js
@@ -3,7 +3,7 @@ const coverageUpdater = require('../index.js');
 console.log('ron coverage-updater.spec.()', coverageUpdater);// TODO remove this
 
 describe('coverage-updater spec', () => {
-  it('should fail', () => {
-    expect(true).toBe(false);
+  it('should pass', () => {
+    expect(true).toBe(true);
   });
 });
