00884f9f89ffd5a095c08bb5a068d8744dc2a793
Ron Apelbaum <ronapelbaum@gmail.com>
2017-04-27 10:24:18 +0300

    import main dep

--- spec/coverage-updater.spec.js
+++ spec/coverage-updater.spec.js
@@ -1,5 +1,5 @@
-// const coverage_updater = require('../index')
-// console.log('ron coverage-updater.spec.()', coverage_updater);// TODO remove this
+const coverage_updater = require('../index.js')
+console.log('ron coverage-updater.spec.()', coverage_updater);// TODO remove this
 
 describe('coverage-updater spec', () => {
   it('should fail', () => {
