923bfc1eec6086a274a576e641d01dd6e09c94a5
Ron Apelbaum <ronapelbaum@gmail.com>
2017-04-30 17:21:11 +0300

    test and lint

--- spec/coverage-updater.spec.js
+++ spec/coverage-updater.spec.js
@@ -1,9 +1,14 @@
-const coverageUpdater = require('../index.js');
-
-console.log('ron coverage-updater.spec.()', coverageUpdater);// TODO remove this
+const coverageUpdater = require('../index.js')['coverage-updater'];
 
 describe('coverage-updater spec', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+  describe('getCoverageSummaryTotal spec', () => {
+    // it('', () => {});
+    it('should return default with zeros', () => {
+      const actual = coverageUpdater.getCoverageSummaryTotal({}, 0);
+      expect(actual.statements).toBe(0);
+      expect(actual.branches).toBe(0);
+      expect(actual.functions).toBe(0);
+      expect(actual.lines).toBe(0);
+    });
   });
 });
