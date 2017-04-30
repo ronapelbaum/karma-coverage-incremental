df6cd9d3bf346b5cab972b0d8a3c9db623838ab7
Ron Apelbaum <ronapelbaum@gmail.com>
2017-04-30 17:55:53 +0300

    add yml support

--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -2,30 +2,37 @@ const utils = require('../index.js').utils;
 const fs = require('fs');
 
 describe('utils spec', () => {
-  describe('getDirectories spec', () => {
-    beforeEach(() => {
-      spyOn(fs, 'existsSync');
-      spyOn(fs, 'readdirSync');
-      spyOn(fs, 'statSync');
-    });
-    it('should empty array if no dir', () => {
-      fs.existsSync.and.returnValue(false);
-      const actual = utils.getDirectories();
-      expect(actual).toEqual([]);
-    });
-    it('should filter not "isDirectory"', () => {
-      const stat1 = jasmine.createSpyObj('Stat', ['isDirectory']);
-      stat1.isDirectory.and.returnValue(true);
-      const stat2 = jasmine.createSpyObj('Stat', ['isDirectory']);
-      const folderContent = [stat1, stat2];
+    describe('getDirectories spec', () => {
+        beforeEach(() => {
+            spyOn(fs, 'existsSync');
+            spyOn(fs, 'readdirSync');
+            spyOn(fs, 'statSync');
+        });
+        it('should empty array if no dir', () => {
+            fs.existsSync.and.returnValue(false);
+            const actual = utils.getDirectories();
+            expect(actual).toEqual([]);
+        });
+        it('should filter not "isDirectory"', () => {
+            const stat1 = jasmine.createSpyObj('Stat', ['isDirectory']);
+            stat1.isDirectory.and.returnValue(true);
+            const stat2 = jasmine.createSpyObj('Stat', ['isDirectory']);
+            const folderContent = [stat1, stat2];
 
-      fs.existsSync.and.returnValue(true);
-      fs.statSync.and.returnValue(true);
-      fs.readdirSync.and.returnValue(folderContent);
+            fs.existsSync.and.returnValue(true);
+            fs.statSync.and.returnValue(true);
+            fs.readdirSync.and.returnValue(folderContent);
 
-      const actual = utils.getDirectories();
+            const actual = utils.getDirectories();
 
-      expect(actual.pop).toBe(stat1);
+            expect(actual.pop).toBe(stat1);
+        });
     });
-  });
+
+    describe('writeYaml spec', () => {
+        it('should empty array if no dir', () => {
+            const actual = utils.writeYaml('.istanbul.yml');
+            expect(actual).toEqual([]);
+        });
+    });
 });
