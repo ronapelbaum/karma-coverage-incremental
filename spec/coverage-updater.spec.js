0d9b5665a3c709c98748591306a37d4e70f1211a
Ron Apelbaum <ronapelbaum@gmail.com>
2017-05-01 00:57:12 +0300

    fix lint

--- spec/coverage-updater.spec.js
+++ spec/coverage-updater.spec.js
@@ -14,9 +14,9 @@ describe('coverage-updater spec', () => {
       const actual = coverageUpdater.getCoverageSummaryTotal({
         total: {
           lines: {
-            pct: 2
-          }
-        }
+            pct: 2,
+          },
+        },
       });
       expect(actual.lines).toBe(2);
     });
@@ -24,42 +24,41 @@ describe('coverage-updater spec', () => {
       const actual = coverageUpdater.getCoverageSummaryTotal({
         total: {
           lines: {
-            pct: 2.2
-          }
-        }
+            pct: 2.2,
+          },
+        },
       });
       expect(actual.lines).toBe(2.2);
     });
     it('should read "pct" value - undefined', () => {
       const actual = coverageUpdater.getCoverageSummaryTotal({
         total: {
-          lines: {}
-        }
+          lines: {},
+        },
       });
       expect(actual.lines).toBe(0);
     });
-
   });
 
   describe('calcCheck spec', () => {
     // it('', () => {});
     it('should return newCheck when value no oldCheck', () => {
       const actual = coverageUpdater.calcCheck({
-        lines: 2
+        lines: 2,
       }, 0);
       expect(actual.lines).toBe(2);
     });
     it('should return newCheck value with flex calc', () => {
       const actual = coverageUpdater.calcCheck({
-        lines: 2
+        lines: 2,
       }, 0.5);
       expect(actual.lines).toBe(1.5);
     });
     it('should return oldCheck value, when higher', () => {
       const actual = coverageUpdater.calcCheck({
-        lines: 2
+        lines: 2,
       }, 0.5, {
-        lines: 1.7
+        lines: 1.7,
       });
       expect(actual.lines).toBe(1.7);
     });
