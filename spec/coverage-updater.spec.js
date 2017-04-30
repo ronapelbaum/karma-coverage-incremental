257625a5533f0612cdcbd318862a6164c4762482
Ron Apelbaum <ronapelbaum@gmail.com>
2017-05-01 00:55:44 +0300

    add test

--- spec/coverage-updater.spec.js
+++ spec/coverage-updater.spec.js
@@ -4,11 +4,64 @@ describe('coverage-updater spec', () => {
   describe('getCoverageSummaryTotal spec', () => {
     // it('', () => {});
     it('should return default with zeros', () => {
-      const actual = coverageUpdater.getCoverageSummaryTotal({}, 0);
+      const actual = coverageUpdater.getCoverageSummaryTotal({});
       expect(actual.statements).toBe(0);
       expect(actual.branches).toBe(0);
       expect(actual.functions).toBe(0);
       expect(actual.lines).toBe(0);
+    });
+    it('should read "pct" value - whole numbers', () => {
+      const actual = coverageUpdater.getCoverageSummaryTotal({
+        total: {
+          lines: {
+            pct: 2
+          }
+        }
+      });
+      expect(actual.lines).toBe(2);
+    });
+    it('should read "pct" value - float numbers', () => {
+      const actual = coverageUpdater.getCoverageSummaryTotal({
+        total: {
+          lines: {
+            pct: 2.2
+          }
+        }
+      });
+      expect(actual.lines).toBe(2.2);
+    });
+    it('should read "pct" value - undefined', () => {
+      const actual = coverageUpdater.getCoverageSummaryTotal({
+        total: {
+          lines: {}
+        }
+      });
+      expect(actual.lines).toBe(0);
+    });
+
+  });
+
+  describe('calcCheck spec', () => {
+    // it('', () => {});
+    it('should return newCheck when value no oldCheck', () => {
+      const actual = coverageUpdater.calcCheck({
+        lines: 2
+      }, 0);
+      expect(actual.lines).toBe(2);
+    });
+    it('should return newCheck value with flex calc', () => {
+      const actual = coverageUpdater.calcCheck({
+        lines: 2
+      }, 0.5);
+      expect(actual.lines).toBe(1.5);
+    });
+    it('should return oldCheck value, when higher', () => {
+      const actual = coverageUpdater.calcCheck({
+        lines: 2
+      }, 0.5, {
+        lines: 1.7
+      });
+      expect(actual.lines).toBe(1.7);
     });
   });
 });
