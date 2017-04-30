5ff5a0397468f7606f825a5d011ea0c8cd07a7d1
Ron Apelbaum <ronapelbaum@gmail.com>
2017-04-30 17:27:21 +0300

    stop auto execution

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const requireIndex = require('requireindex');
 const lib = requireIndex(`${__dirname}/lib/`);
 module.exports = lib;
 
-lib['coverage-updater'].updateCodeCoverage();
+// lib['coverage-updater'].updateCodeCoverage();
