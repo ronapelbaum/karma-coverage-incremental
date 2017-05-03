ff9d2e16b3d492940a89bd5547ae3ad753f11222
Ron Apelbaum <ronapelbaum@gmail.com>
2017-05-03 09:18:38 +0300

    lint

--- spec/karma-plugin.spec.js
+++ spec/karma-plugin.spec.js
@@ -3,8 +3,8 @@ const coverageUpdater = require('../lib/coverage-updater');
 
 describe('incrementReporter spec', () => {
   const coverageReporter = 'coverage';
-  const htmlCoverageReporter = {type: 'html'};
-  const jsonSummaryCoverageReporter = {type: 'json-summary'};
+  const htmlCoverageReporter = { type: 'html' };
+  const jsonSummaryCoverageReporter = { type: 'json-summary' };
 
   describe('export spec', () => {
     it('should export properly', () => {
@@ -24,7 +24,7 @@ describe('incrementReporter spec', () => {
         expect(() => incrementReporter(config)).toThrow(new Error('coverage reporter should be used'));
       });
       it('should NOT throw error if "coverage" reporter defined', () => {
-        const config = {reporters: [coverageReporter]};
+        const config = { reporters: [coverageReporter] };
         expect(() => incrementReporter(config)).not.toThrow(new Error('coverage reporter should be used'));
       });
     });
@@ -39,7 +39,7 @@ describe('incrementReporter spec', () => {
         expect(() => incrementReporter(config)).toThrow(new Error(errMsg));
       });
       it('should throw error if no "json-summary" coverage-reporter defined as a reporter in reporters array', () => {
-        config.coverageReporter = {reporters: [htmlCoverageReporter]};
+        config.coverageReporter = { reporters: [htmlCoverageReporter] };
         expect(() => incrementReporter(config)).toThrow(new Error(errMsg));
       });
       it('should NOT throw error if "json-summary" coverage-reporter defined as main reporter', () => {
@@ -47,17 +47,18 @@ describe('incrementReporter spec', () => {
         expect(() => incrementReporter(config)).not.toThrow(new Error(errMsg));
       });
       it('should NOT throw error if "json-summary" coverage-reporter defined as a reporter in reporters array', () => {
-        config.coverageReporter = {reporters: [htmlCoverageReporter, jsonSummaryCoverageReporter]};
+        config.coverageReporter = {
+          reporters: [htmlCoverageReporter, jsonSummaryCoverageReporter],
+        };
         expect(() => incrementReporter(config)).not.toThrow(new Error(errMsg));
       });
     });
   });
 
   describe('use of coverageUpdater spec', () => {
-
     let config;
-    const incrementConfigDefault = {isKarma: true};
-    const incrementConfig = {flexibility: 0.3};
+    const incrementConfigDefault = { isKarma: true };
+    const incrementConfig = { flexibility: 0.3 };
 
     beforeEach(() => {
       spyOn(coverageUpdater, 'updateCodeCoverage');
@@ -73,7 +74,8 @@ describe('incrementReporter spec', () => {
     it('should call updateCodeCoverage()', () => {
       config.incrementConfig = incrementConfig;
       incrementReporter(config);
-      expect(coverageUpdater.updateCodeCoverage).toHaveBeenCalledWith(Object.assign(incrementConfig, incrementConfigDefault));
+      const params = Object.assign(incrementConfig, incrementConfigDefault);
+      expect(coverageUpdater.updateCodeCoverage).toHaveBeenCalledWith(params);
     });
   });
 });
