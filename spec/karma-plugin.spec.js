17d356172df70e50e8024a5fe201177e0c1eaa89
Ron Apelbaum <ronapelbaum@gmail.com>
2017-05-03 09:05:14 +0300

    add tests

--- spec/karma-plugin.spec.js
+++ spec/karma-plugin.spec.js
@@ -13,23 +13,41 @@ describe('incrementReporter spec', () => {
   describe('requirements spec', () => {
     describe('coverage reporter', () => {
       it('should throw error if no "coverage" reporter defined', () => {
-        expect(() => incrementReporter({})).toThrow(new Error('coverage reporter should be used'));
+        const config = {};
+        expect(() => incrementReporter(config)).toThrow(new Error('coverage reporter should be used'));
+        config.reporters = ['aaa'];
+        expect(() => incrementReporter(config)).toThrow(new Error('coverage reporter should be used'));
+      });
+      it('should NOT throw error if "coverage" reporter defined', () => {
+        const config = {reporters: ['coverage']};
+        expect(() => incrementReporter(config)).not.toThrow(new Error('coverage reporter should be used'));
       });
     });
     describe('json-summary coverage-reporter', () => {
       const coverageReporter = { type: 'html' };
+      const jsonSummaryReporter = { type: 'json-summary' };
       const config = {
         reporters: ['coverage'],
       };
       const errMsg = 'json-summary coverage-reporter should be used';
-      it('should throw error if no "json-summary" coverage-reporter defined', () => {
+
+      it('should throw error if no "json-summary" coverage-reporter defined as main reporter', () => {
         config.coverageReporter = coverageReporter;
         expect(() => incrementReporter(config)).toThrow(new Error(errMsg));
       });
-      it('should throw error if no "coverage" reporter defined', () => {
-        config.coverageReporter = [coverageReporter];
+      it('should throw error if no "json-summary" coverage-reporter defined as a reporter in reporters array', () => {
+        config.coverageReporter = {reporters: [coverageReporter]};
         expect(() => incrementReporter(config)).toThrow(new Error(errMsg));
       });
+      it('should NOT throw error if "json-summary" coverage-reporter defined as main reporter', () => {
+        config.coverageReporter = jsonSummaryReporter;
+        expect(() => incrementReporter(config)).not.toThrow(new Error(errMsg));
+      });
+      it('should NOT throw error if "json-summary" coverage-reporter defined as a reporter in reporters array', () => {
+        config.coverageReporter = {reporters: [coverageReporter, jsonSummaryReporter]};
+        expect(() => incrementReporter(config)).not.toThrow(new Error(errMsg));
+      });
+
     });
   });
 });
